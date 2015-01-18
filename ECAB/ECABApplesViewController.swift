7cdfcc5bd027c6737a47b34e1a15c3fd7f1bfd22
Boris Yurkevich <boris.yurkevich@manything.com>
2015-01-18 12:18:24 +0000

    [Refactoring] Renamed SecondVC to ApplesVC

---
+++ ECAB/ECABApplesViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ECABApplesViewController.swift
+//  ECAB
+//
+//  Created by Boris Yurkevich on 18/01/2015.
+//  Copyright (c) 2015 Oliver Braddick and Jan Atkinson. All rights reserved.
+//
+
+import UIKit
+
+class ECABApplesViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
