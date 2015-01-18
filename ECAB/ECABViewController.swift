b31913e9e02bd61573b68fd4bfde21c3a114e0f4
Boris Yurkevich <boris.yurkevich@manything.com>
2015-01-18 22:30:34 +0000

    [UI] Start game button Added empty ECABVC

---
+++ ECAB/ECABViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ECABViewController.swift
+//  ECAB
+//
+//  Generic class for subclassing, provides basic navigation bar
+//  and link to the model
+//
+//  Created by Boris Yurkevich on 18/01/2015.
+//  Copyright (c) 2015 Oliver Braddick and Jan Atkinson. All rights reserved.
+//
+
+import UIKit
+
+class ECABViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
