7096c79090d28061c36b97e0b1e1d43615111170
Boris Yurkevich <boris.yurkevich@manything.com>
2015-01-18 11:51:33 +0000

    Initial Commit

---
+++ ECAB/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ECAB
+//
+//  Created by Boris Yurkevich on 18/01/2015.
+//  Copyright (c) 2015 Oliver Braddick and Jan Atkinson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
