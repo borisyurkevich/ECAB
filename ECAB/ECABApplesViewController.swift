2d465b7efa896c5504d19e023d988e203d5f7d41
Boris Yurkevich <boris.yurkevich@gmail.com>
2015-02-08 21:46:20 +0000

    This is no longer needed, thanks to Storyboard.

--- ECAB/ECABApplesViewController.swift
+++ ECAB/ECABApplesViewController.swift
@@ -15,18 +15,6 @@ class ECABApplesViewController: UIViewController, SubjectPickerDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    @IBAction func openPopUp(sender: UIBarButtonItem) {
-        let popOverContentVC = ECABSubjectPickerDataSourceTVC()
-        let popOver = UIPopoverController(contentViewController: popOverContentVC)
-        
-        popOverContentVC.delegate = self
-
-        popOverContentVC.modalPresentationStyle = .Popover
-        popOverContentVC.preferredContentSize = CGSizeMake(200, 140)
-        
-        popOver.presentPopoverFromBarButtonItem(sender, permittedArrowDirections: UIPopoverArrowDirection(1), animated: true)
-    }
     
     func pickSubject(isDefault: Bool) {
         self.performSegueWithIdentifier("startApplesGame", sender: self)
@@ -48,8 +36,6 @@ class ECABApplesViewController: UIViewController, SubjectPickerDelegate
     // MARK: - Navigation
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
         if segue.destinationViewController.isKindOfClass(ECABSubjectPickerDataSourceTVC) {
             var popOver = segue.destinationViewController as ECABSubjectPickerDataSourceTVC
             popOver.delegate = self
