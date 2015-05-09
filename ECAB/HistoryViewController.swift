62191a7aea79122711cba6d6fcaf514723f8c5a8
Boris Yurkevich <boris.yurkevich@gmail.com>
2015-05-09 12:58:19 +0100

    Refactored.

--- ECAB/HistoryViewController.swift
+++ ECAB/HistoryViewController.swift
@@ -10,53 +10,14 @@ import UIKit
 
 class HistoryViewController: UIViewController {
 
-    @IBOutlet var labels: [UILabel]!
-    @IBOutlet weak var ageLabel: UILabel!
-    @IBOutlet weak var gameNameLabel: UILabel!
     @IBOutlet weak var scoresLabel: UILabel!
-    
-    var currentSubject: Player?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        currentSubject = Model.sharedInstance.subject
-        update()
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        update()
     }
-    
-    func update() {
-        
-        var name: String
-        var surname: String
-        var age: Int
-        
-        if currentSubject?.name != nil {
-            name = currentSubject!.name
-        } else {
-            name = "New"
-        }
-        if currentSubject?.surname != nil {
-            surname = currentSubject!.surname
-        } else {
-            surname = "Player"
-        }
-        if (currentSubject?.age != nil) {
-            age = currentSubject!.age
-            ageLabel.text = String(age)
-        } else {
-            ageLabel.text = "Unknown"
-        }
-        
-        self.title = "\(name) \(surname)"
-        
-        if (currentSubject?.sessions.last != nil) {
-            let session = currentSubject?.sessions.last
-            let score = session!.score
-            scoresLabel.text = String(score.scores)
-        }
-    }
+
 }
