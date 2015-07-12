5224c0088ba15ba12c4dafea2d93459fa00eaa39
Boris Yurkevich <boris.yurkevich@gmail.com>
2015-07-12 11:45:21 +0100

    Revert "Generated classed for Core Data."
    This reverts commit 688317b6837991ae9158f5a76f9f6ca33879417b.

--- ECAB/Data.swift
+++ ECAB/Data.swift
@@ -2,7 +2,7 @@
 //  Data.swift
 //  ECAB
 //
-//  Created by Boris Yurkevich on 11/07/2015.
+//  Created by Boris Yurkevich on 6/10/15.
 //  Copyright (c) 2015 Oliver Braddick and Jan Atkinson. All rights reserved.
 //
 
@@ -13,12 +13,9 @@ class Data: NSManagedObject {
 
     @NSManaged var id: String
     @NSManaged var selectedGame: NSNumber
-    @NSManaged var visualSearchDifficultyEasy: NSNumber
-    @NSManaged var visualSearchSpeed: NSNumber
-    @NSManaged var visualSustSpeed: NSNumber
-    @NSManaged var counterpointingSessions: NSOrderedSet
     @NSManaged var players: NSOrderedSet
     @NSManaged var selectedPlayer: Player
     @NSManaged var sessions: NSOrderedSet
+    @NSManaged var counterpointingSessions: NSOrderedSet
 
 }
--- ECAB/Session.swift
+++ ECAB/Session.swift
@@ -2,7 +2,7 @@
 //  Session.swift
 //  ECAB
 //
-//  Created by Boris Yurkevich on 11/07/2015.
+//  Created by Boris Yurkevich on 6/10/15.
 //  Copyright (c) 2015 Oliver Braddick and Jan Atkinson. All rights reserved.
 //
 
@@ -12,14 +12,9 @@ import CoreData
 class Session: NSManagedObject {
 
     @NSManaged var dateStart: NSDate
-    @NSManaged var extraMode: NSNumber
-    @NSManaged var extraSpeed: NSNumber
     @NSManaged var failureScore: NSNumber
-    @NSManaged var normalSpeed: NSNumber
     @NSManaged var repeatCount: NSNumber
     @NSManaged var score: NSNumber
-    @NSManaged var comment: String
-    @NSManaged var speed: NSNumber
     @NSManaged var data: Data
     @NSManaged var moves: NSOrderedSet
     @NSManaged var player: Player
