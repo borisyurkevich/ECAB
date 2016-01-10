0465cd39b134622e2fc4b426bda6737b3843ad89
Boris Yurkevich <boris.yurkevich@gmail.com>
2016-01-10 15:49:43 +0000

    Add moves to the visual search CSV file

--- ECAB/DataExportModel.swift
+++ ECAB/DataExportModel.swift
@@ -34,7 +34,7 @@ class DataExportModel {
 	}
 	
 	func createVisualSearchTable() -> String? {
-		var returnValue: String? = nil
+		var returnValue = "empty line\n"
 		
 		if let visualSearchSession: Session = pickedVisualSearchSession {
 			let gameName = model.games[Int(model.data.selectedGame)]
@@ -67,25 +67,99 @@ class DataExportModel {
 			              "date of birth           ,\(birth)       ,age at test   ,\(age)         ,               ,               ,               \n" +
 						  "date/time of test start ,\(dateStart)   ,\(timeStart)  ,               ,               ,               ,               \n" +
 						  "                        ,               ,              ,               ,               ,               ,               \n" +
-						  "parameters              ,\(difficulty)  ,              ,\(speed) s     ,               ,               ,               \n" +
+						  "parameters              ,\(difficulty)  ,              ,               ,\(speed) s     ,               ,               \n" +
 						  "comments                ,\(comments)    ,              ,               ,               ,               ,               \n" +
 						  "                        ,               ,              ,               ,               ,               ,               \n" +
 						  "                        ,               ,              ,               ,               ,               ,               \n" +
-						  "                        ,               ,              ,motor 1        ,motor 2        ,motor 3        ,TOTAL          \n" +
+						  "                        ,               ,motor 1       ,motor 2        ,motor 3        ,TOTAL          ,*              \n" +
 						  "no of hits              ,               ,              ,               ,               ,               ,               \n" +
 						  "no of false positives   ,               ,              ,               ,               ,               ,               \n" +
- 						  "total time              ,               ,              ,               ,               ,               ,               \n" +
+ 						  "total time              ,               ,              ,               ,               ,               ,**             \n" +
 						  "                        ,               ,              ,               ,               ,               ,               \n" +
 						  "                        ,               ,              ,               ,               ,               ,               \n" +
-					      "                        ,               ,              ,search 1       ,search 2       ,search 3       ,               \n" +
+					      "                        ,               ,search 1      ,search 2       ,search 3       ,               ,               \n" +
   						  "no of hits              ,               ,              ,               ,               ,               ,               \n" +
 						  "no of false positives   ,               ,              ,               ,               ,               ,               \n" +
-						  "total time              ,               ,              ,               ,               ,               ,               \n" +
+						  "total time              ,               ,              ,               ,               ,               ,**             \n" +
 			              "hits- false positives   ,               ,              ,               ,               ,               ,               \n" +
   						  "                        ,\(screenComm)  ,              ,               ,               ,               ,               \n" +
 						  "                        ,\(durationComm),              ,               ,               ,               ,               \n" +
 						  "                        ,               ,              ,               ,               ,               ,               \n" +
-						  "\(header)               ,               ,              ,               ,               ,               ,               \n"
+						  "\(header)               ,               ,              ,               ,               ,               ,               \n" +
+                          "                        ,               ,target row    ,target col     ,time           ,               ,               \n"
+            
+            // Append dynamic lines
+            for move in visualSearchSession.moves {
+                let gameMove = move as! Move
+                let screenNumber = gameMove.screenNumber.integerValue
+                
+                var ignoreThisMove = false
+                switch screenNumber {
+                case 0 ... 2, 11 ... 13:
+                    ignoreThisMove = true // Training
+                default:
+                    break
+                }
+                if !ignoreThisMove {
+                    if gameMove.empty.boolValue == false {
+                        // Success or failure
+                        var sof = ""
+                        if gameMove.success.boolValue == true {
+                            sof = "hit"
+                        } else {
+                            sof = "false"
+                        }
+                        // ve / repeat
+                        var veor = ""
+                        if gameMove.`repeat`.boolValue == true {
+                            veor = "repeat"
+                        } else {
+                            veor = "ve"
+                        }
+                        // time
+                        let moveTime = timeFormatter.stringFromDate(gameMove.date)
+                        // Row and column
+                        let targetRow = gameMove.row
+                        let targetColumn = gameMove.column
+                        
+                        // CSV line
+                        let line = ",\(sof) \(veor), \(targetRow), \(targetColumn), \(moveTime)\n"
+                        returnValue += line
+                    } else {
+                        var header = "header uknown"
+                        switch screenNumber {
+                        case 3:
+                            header = "motor screen 1"
+                        case 4:
+                            header = "motor screen 2"
+                        case 5:
+                            header = "motor screen 3"
+                        case 14:
+                            header = "motor screen 1"
+                        case 15:
+                            header = "motor screen 2"
+                        case 6:
+                            header = "search screen 1"
+                        case 7:
+                            header = "search screen 2"
+                        case 8:
+                            header = "search screen 3"
+                        case 16:
+                            header = "search screen 1"
+                        case 17:
+                            header = "search screen 2"
+                        default:
+                            header = "wrong header number"
+                        }
+                        // Time
+                        let headerTime = timeFormatter.stringFromDate(gameMove.date)
+                        
+                        // CSV line
+                        let headerLine = "\(header),screen onset, , ,\(headerTime)\n"
+                        returnValue += headerLine
+                    }
+                }
+            }
 		}
 		
 		return returnValue
